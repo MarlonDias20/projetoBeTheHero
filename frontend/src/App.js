7e5be0265c6ad8386cb9343a29df24947c0cbe72
Marlon <marlon@webdec.com.br>
2020-11-19 00:09:50 -0300

    Definição de primeiros conceitos, limpeza do ambiente e conversão de tabulação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Header from './Header';
+
+// JSX (JavaScript XML)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello BeTheHero
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+
+	// Array [valor, funcaoDeAtualizacao]
+
+	function increment() {
+		setCount(count + 1);
+	}
+
+	return (
+		<div>
+			<Header title={"Contador: " + count} />
+			
+			<button onClick={increment}>Incrementar</button>
+		</div>
+	);
 }
 
 export default App;
