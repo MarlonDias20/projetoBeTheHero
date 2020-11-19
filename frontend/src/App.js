95c7008bf154daf60874e54a060b32f64a20b49f
Marlon <marlon@webdec.com.br>
2020-11-19 00:12:20 -0300

    Ajuste tabulação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,21 @@ import Header from './Header';
 // JSX (JavaScript XML)
 
 function App() {
-	const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-	// Array [valor, funcaoDeAtualizacao]
+    // Array [valor, funcaoDeAtualizacao]
 
-	function increment() {
-		setCount(count + 1);
-	}
+    function increment() {
+        setCount(count + 1);
+    }
 
-	return (
-		<div>
-			<Header title={"Contador: " + count} />
-			
-			<button onClick={increment}>Incrementar</button>
-		</div>
-	);
+    return (
+        <div>
+            <Header title={"Contador: " + count} />
+            
+            <button onClick={increment}>Incrementar</button>
+        </div>
+    );
 }
 
 export default App;
